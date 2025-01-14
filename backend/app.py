ee297952b97b0022252893a7450fdda224ec7f92
Lee Eames <lceames@gmail.com>
2025-01-14 12:28:15 -0500

    change port

--- backend/app.py
+++ backend/app.py
@@ -181,5 +181,5 @@ def serve_index():
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5005))
     app.run(host="0.0.0.0", port=port)
